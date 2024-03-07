3f745316080ac421d80aeac85cdcbdedc3947486
ringo kwon <ringo.kwon@gmail.com>
2024-03-07 13:56:50 +0900

    hone ongoing

---
+++ adv_basics_sol/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:first_app_sol/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 59, 17, 131), 
+              Color.fromARGB(255, 120, 25, 114)
+            ]
+          ),
+        ),
+        child: const StartScreen(),
+      ),
+    ),
+  ));
+}
